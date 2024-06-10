425a4fd3bc88e1f3897a22726b67b6564dfa76f7
Eduardo Palhares Reale Pereira <130712646+EduardoPalharesRealePereira@users.noreply.github.com>
2024-06-09 22:34:24 -0300

    Add files via upload

---
+++ codigo/src/main/java/com/advanced/repositories/ItemCardapioRepository.java
@@ -0,0 +1,9 @@
+package com.advanced.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.advanced.entities.ItemCardapio;
+
+public interface ItemCardapioRepository extends JpaRepository<ItemCardapio, Long> {
+
+}
