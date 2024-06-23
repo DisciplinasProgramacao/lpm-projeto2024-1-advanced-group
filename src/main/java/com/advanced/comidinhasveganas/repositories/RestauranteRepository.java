f0a884273034ade59d6d1380497a7fb364d9574f
lucaslmchaves <lucaslmchaves@gmail.com>
2024-06-23 15:58:23 -0300

    create: classe mesa documentada + exceptions +runner e arrumando estrutura de pastas e xml do projeto

--- src/main/java/com/advanced/comidinhasveganas/repositories/RestauranteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.advanced.comidinhasveganas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.advanced.comidinhasveganas.entities.Restaurante;
-
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
-
-}
