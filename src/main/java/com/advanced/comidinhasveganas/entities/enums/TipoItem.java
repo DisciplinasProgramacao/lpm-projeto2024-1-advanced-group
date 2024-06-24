90937a40a00dfe517e3243b4685ce91069f316d4
lucaslmchaves <lucaslmchaves@gmail.com>
2024-06-24 17:20:29 -0300

    create: Factory de Pedido (feature futura da aplciação principal)

--- src/main/java/com/advanced/comidinhasveganas/entities/enums/TipoItem.java
+++
@@ -1,6 +0,0 @@
-package com.advanced.comidinhasveganas.entities.enums;
-
-public enum TipoItem {
-  COMIDA,
-  BEBIDA
-}
--- src/main/java/com/advanced/comidinhasveganas/repositories/ItemPedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.advanced.comidinhasveganas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.advanced.comidinhasveganas.entities.ItemPedido;
-
-public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
-
-}
